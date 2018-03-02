30d13a3c4bbf45c13cf4e41d10041f86f554b6e0
Juan Pablo <programmeringpablo@outlook.com>
2018-03-01 19:49:34 -0600

    se agrego el componente de registro y el de acceso al sitio

--- front-end/beatsapp/src/app/app.module.ts
+++ front-end/beatsapp/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RoutingModule } from './routes';
+import { RouterLink } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { SingupComponent } from './components/singup/singup.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TopbarComponent
+    TopbarComponent,
+    SingupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/beatsapp/src/app/routes.ts
+++ front-end/beatsapp/src/app/routes.ts
@@ -8,9 +8,11 @@
 
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SingupComponent } from './components/singup/singup.component';
 
 const RoutesArray: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'singup', component: SingupComponent },
     { path: '**',   pathMatch: 'full', redirectTo: 'home' }
 ];
 
