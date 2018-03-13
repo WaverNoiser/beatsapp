694e63a1be035d5d71e3cf47763a3ef93feee16c
Juan Pablo <programmeringpablo@outlook.com>
2018-03-12 21:08:57 -0600

    se agrego la funcionalidad al singin

--- front-end/beatsapp/src/app/components/signin/signin.component.ts
+++ front-end/beatsapp/src/app/components/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -9,16 +10,42 @@ import { UserService } from '../../services/user.service';
 
 })
 export class SigninComponent implements OnInit {
-  public response;
+  public existUser = true;
 
-  constructor( private userService: UserService) { }
+  constructor( private userService: UserService, private router: Router ) { }
 
   ngOnInit() {
   }
 
-  ingresar( nickName: string ) {
-    console.log( 'este es el nombre: ', nickName );
-    this.userService.getUser( nickName ).subscribe( res => this.response = Object.values( res )[0].length ===  2  );
+  ingresar( nickName: string, password: string ) {
+    let obj;
+    this.userService.getUser( nickName ).subscribe( res => {
+       obj = JSON.parse( Object.values( res )[0]  );
+
+       /* si los datos del que intenta acceder son iguales a los de
+      traidos por la BD en funcion al 'nickName' */
+      try {
+      this.existUser = (obj[0].nickName === nickName) && ( this.equals( obj[0].password , password )  );
+      if ( this.existUser ) {
+        console.log('aqui');
+        this.router.navigate(['/home']);
+      }
+      } catch (err) {
+        this.existUser = false;
+      }
+      console.log( this.existUser );
+    }  );
   }
+
+  // funcion para determinar si una cadena es igual a la otra
+
+equals( str1: string, str2: string ): boolean {
+  if ( str1.indexOf(str2) !== -1 ) {
+    if ( str1.length === str2.length ) {
+      return true;
+    }
+  }
+  return false;
+}
 
 }
